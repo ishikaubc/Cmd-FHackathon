2490dd5243744a0b111d97f595320d690bdb27af
Mina Chong <mina.s.chong@gmail.com>
2023-03-11 14:29:53 -0800

    Added login.php and signup.php to set up skeleton for sign in pages

---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir ="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>LifeConnect</title>
+    </head>
+    <body>
+        <nav>
+            <div class="wrapper">
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="signup.php">Sign Up</a></li>
+                    <li><a href="login.php">Login</a></li>
+                </ul>
+            </div>
+        </nav>
+
+<div class="wrapper">
