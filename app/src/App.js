e02a0273c7bc21d9d279352ff8c33f732fb25121
Joyce <joycechen1007@gmail.com>
2023-03-11 15:10:16 -0800

    added header, section 1 and 2

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Header from './components/Header';
+import Section1 from './components/Section1';
+import Section2 from './components/Section2';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='homePage'>
+      <Header />
+      <div className='frame'>
+        <Section1 />
+        <Section2 />
+      </div>
     </div>
   );
 }
---
+++ app/src/components/Section1.js
@@ -0,0 +1,19 @@
+import './Section1.css'
+
+function Section1() {
+    return (
+        <div>
+            <h2>About US</h2>
+
+            <h2>Guidelines</h2>
+
+            <h2>Tips to follow</h2>
+
+            <h2>registration</h2>
+
+            <h2>Contact US</h2>
+        </div>
+    );
+}
+
+export default Section1;
