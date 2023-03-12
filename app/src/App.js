4a67eb0c2dd37bbae056537a204cfd59351bd43a
ishikaubc <ishikaubc@gmail.com>
2023-03-12 10:03:25 -0700

    finaalchanges

--- app/src/App.js
+++ app/src/App.js
@@ -4,6 +4,49 @@ import ReactDOM from 'react-dom/client';
 import TopNav from './components/TopNav';
 import Section1 from './components/Section1';
 import Section2 from './components/Section2';
+import ChatBot from 'react-simple-chatbot';
+import { ThemeProvider } from 'styled-components';
+
+const steps = [
+  {
+    id: '0',
+    message: 'Hey User',
+    end: true
+  },
+  {
+    id: '1',
+
+    // This message appears in
+    // the bot chat bubble
+    message: 'How can I help you today',
+    trigger: '2'
+}, {
+    id: '2',
+
+    // Here we want the user
+    // to enter input
+    user: true,
+    trigger: '3',
+}, {
+    id: '3',
+    message: " I am doing all good. I want your help",
+    trigger: 4
+}, {
+    id: '4',
+    options: [
+         
+        // When we need to show a number of
+        // options to choose we create alist
+        // like this
+        { value: 1, label: 'Donate Blood' },
+        { value: 2, label: 'Find a donor' },
+
+    ],
+    end: true
+}
+];
+
+
 
 
 export default function App() {
@@ -14,9 +57,18 @@ export default function App() {
         <div className='container1'>
           <Section1 />
           <Section2 />
-        </div>
-    </div>
+     
+      <div className = "App">
+        <h1>Welcome to LifeConnect</h1>
+        <ChatBot steps = {steps}/>
+      </div>
+      </div>
+      </div>
+    )
+  }
 
-  )
-}
 
+ 
+
+ 
+        
--- chatbot.py
+++ chatbot.py
@@ -1,50 +1,47 @@
 
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 from random import choice
-from flask import Flask, request
+#pythfrom flask import Flask, request
 
 import os
 import openai
-load_dotenv()
-openai.api_key = "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"
+# load_dotenv()
+openai.api_key = "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"
 completion = openai.Completion()
 
-response = openai.Completion.create(
-  model="text-davinci-003",
-  prompt="Hello, I am your blood donor buddy. I will help you to know if you are fit to donate blood or not\n\nUser: Hi, I want to register to donate blood\nBot:   Okay, sounds good. What is your age?\n\nUser: I am 20 years old. \nBot: Okay, may I ask if you had any past medical history?\n\nUser: No, I am all good.\nBot: Okay, did you get a tattoo done within past 12 month?\n\nUser: No.\nBot:  You are eligible to donate blood.  Please contact your local blood bank to schedule an appointment. Thank you for your willingness to contribute to this noble cause.\n\n\n\n\n\n",
-  temperature=0.7,
-  max_tokens=256,
-  top_p=1,
-  frequency_penalty=0,
-  presence_penalty=0
-)
-start_sequence = "\nBot:"
-restart_sequence = "\n\nUser:"
-session_prompt = "Hello, I am your blood donor buddy. I will help you to know if you are fit to donate blood or not\n\nUser:Hi,I want to register to donate blood\nBot:Okay, sounds good. What is your age?\n\nUser:I am 20 years old.\nBot:Okay, may I ask if you had any past medical history?\n\nUser:No, I am all good.\nBot:Okay, did you get a tattoo done within past 12 month?\n\nUser:No.\nBot:You are eligible to donate blood.  Please contact your local blood bank to schedule an appointment. Thank you for your willingness to contribute to this noble cause."
+# response = openai.Completion.create(
+#   model="text-davinci-003",
+#   prompt="Hello, I am your blood donor buddy. I will help you to know if you are fit to donate blood or not\n\nUser: Hi, I want to register to donate blood\nBot:   Okay, sounds good. What is your age?\n\nUser: I am 20 years old. \nBot: Okay, may I ask if you had any past medical history?\n\nUser: No, I am all good.\nBot: Okay, did you get a tattoo done within past 12 month?\n\nUser: No.\nBot:  You are eligible to donate blood.  Please contact your local blood bank to schedule an appointment. Thank you for your willingness to contribute to this noble cause.\n\n\n\n\n\n",
+#   temperature=0.7,
+#   max_tokens=256,
+#   top_p=1,
+#   frequency_penalty=0,
+#   presence_penalty=0
+# )
+# start_sequence = "\nBot:"
+# restart_sequence = "\n\nUser:"
+# session_prompt = "Hello, I am your blood donor buddy. I will help you to know if you are fit to donate blood or not\n\nUser:Hi,I want to register to donate blood\nBot:Okay, sounds good. What is your age?\n\nUser:I am 20 years old.\nBot:Okay, may I ask if you had any past medical history?\n\nUser:No, I am all good.\nBot:Okay, did you get a tattoo done within past 12 month?\n\nUser:No.\nBot:You are eligible to donate blood.  Please contact your local blood bank to schedule an appointment. Thank you for your willingness to contribute to this noble cause."
 
-<<<<<<< HEAD
-openai.api_key = os.getenv("") # replace with your API key
-=======
->>>>>>> 0b8982d5f9533c0ebe69779fd2d383fd5a1cdfb6
+# openai.api_key = os.getenv("") # replace with your API key
 
-
-def ask(question, chat_log=None):
-    prompt_text = f'{chat_log}{restart_sequence}: {question}{start_sequence}:'
-    response = openai.Completion.create(
-      engine="davinci",
-      temperature=0.7,
-      max_tokens=150,
-      top_p=1,
-      frequency_penalty=0,
-      presence_penalty=0.3,
-      stop=["\n"],
-    )
-    return print(response.choices[0].text)
+def ask(question, chatHistory):
+  history = '\n'.join(chatHistory)
+  prompt_text = f"{history}\nUser: {question}"
+  response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt=prompt_text,
+    max_tokens=2048,
+    top_p=1,
+  )
+  response = response.choices[0].text
+  print(response)
+  chatHistory.append(chatHistory)
  
 def main():
-    while True:
-        print('GPT:Ask me a question\n')
-        myOn = input()
-        ask(myOn)
-        print('\n')
+  initialPrompt = "Pretend you are a blood donor diagnosis chat bot. Ask the user questions about their age and health. The user will indicate their responses in the format User: <response>. Respond in the format Bot: <response>. Begin the conversation."
+  chatHistory = [initialPrompt, "Bot: Hi! How can I help you?"]
+  print("\n".join(chatHistory))
+  while True:
+      myOn = input()
+      ask(myOn, chatHistory)
 main()
